46635355f4373e3729c33192f2b0d075e9e1fbd2
Mudaser <mudaserrahman7@gmail.com>
2026-07-12 16:29:44 +0500

    now complete the repoimpl

---
+++ lib/features/auth/data/repositories/auth_repositoriesimpl.dart
@@ -0,0 +1,109 @@
+import 'package:cofe_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:dartz/dartz.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../../../../core/ error/exceptions.dart';
+import '../../../../core/ error/failure.dart';
+import '../../domain/entity/user_entity.dart';
+import '../../domain/repository/auth_repository.dart';
+
+
+
+class AuthRepositoryImpl implements AuthRepository {
+   final AuthRemoteDataSource authRemoteDataSource;
+
+  AuthRepositoryImpl(this.authRemoteDataSource);
+
+  @override
+  Future<Either<Failure, UserEntity>> signUp({
+    required String name,
+    required String email,
+    required String password,
+
+
+  }) async {
+    try {
+      final user = await authRemoteDataSource.signUp(
+        name: name,
+        email: email,
+        password: password,
+      );
+      return right(user);
+    } on ServerException catch (e) {
+      return left(
+        ServerFailure(e.message),
+      );
+    } on CacheException catch (e) {
+      return left(
+        CacheFailure(e.message),
+      );
+    } on BadRequestException catch (e) {
+      return left(
+        BadRequestFailure(e.message),
+      );
+    } on NotFoundException catch (e) {
+      return left(
+        NotFoundFailure(e.message),
+      );
+    } on ConflictException catch (e) {
+      return left(
+        ConflictFailure(e.message),
+      );
+    }
+  }
+
+
+  @override
+  Future<Either<Failure, UserEntity>>signIn({
+    required String email,
+    required String password,
+
+})
+  async {
+    try {
+      final user = await authRemoteDataSource.signIn(
+        email: email,
+        password: password,
+      );
+      return Right(user as UserEntity);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(e.message));
+    } on CacheException catch (e) {
+      return Left(CacheFailure(e.message));
+    } on BadRequestException catch (e) {
+      return Left(BadRequestFailure(e.message));
+    } on NotFoundException catch (e) {
+      return Left(NotFoundFailure(e.message));
+    } on ConflictException catch (e) {
+      return Left(ConflictFailure(e.message));
+    } on ValidationException catch (e) {
+      return Left(ValidationFailure(e.message));
+    }
+  }
+    @override
+    Future<Either<Failure, void>> signOut() async {
+      try {
+        await authRemoteDataSource.signOut();
+
+        return const Right(null);
+      } on AuthException catch (e) {
+        return Left(UnauthorizedFailure(e.message));
+      } catch (e) {
+        return Left(UnknownFailure(e.toString()));
+      }
+    }
+    @override
+    Future<Either<Failure, UserEntity>> getCurrentUser() async {
+      try {
+        final user = await authRemoteDataSource.getCurrentUser();
+
+        return Right(user as UserEntity);
+      } on PostgrestException catch (e) {
+        return Left(DatabaseFailure(e.message));
+      } on AuthException catch (e) {
+        return Left(UnauthorizedFailure(e.message));
+      } catch (e) {
+        return Left(UnknownFailure(e.toString()));
+      }
+    }
+  }
