365768582be15c943e6470ba6bc9cad43c8a3d6d
Mudaser <mudaserrahman7@gmail.com>
2026-07-13 12:35:56 +0500

    now add event

--- lib/features/auth/presentation/auth_event.dart
+++ lib/features/auth/presentation/auth_event.dart
@@ -1,4 +1,44 @@
-part of 'auth_bloc.dart';
+import 'package:equatable/equatable.dart';
 
-@immutable
-sealed class AuthEvent {}
+class AuthEvent extends Equatable {
+  const AuthEvent();
+  @override
+  List<Object> get props =>[];
+}
+class SignUpEvent extends AuthEvent {
+  final String name;
+  final String email;
+  final String password;
+  final String? photoUrl;
+  const SignUpEvent({
+
+    required this.name,
+    required this.email,
+    required this.password,
+    this.photoUrl,
+  });
+  @override
+  List<Object> get props => [name, email, password];
+}
+class SignInEvent extends AuthEvent {
+  final String email;
+  final String password;
+  const SignInEvent({
+    required this.email,
+    required this.password,
+  });
+  @override
+  List<Object> get props => [email, password];
+}
+class SignOutEvent extends AuthEvent {
+  const SignOutEvent();
+  @override
+  List<Object> get props => [];
+}
+class GetCurrentUserEvent extends AuthEvent {
+  const GetCurrentUserEvent();
+  @override
+  List<Object> get props => [];
+}
+
+
