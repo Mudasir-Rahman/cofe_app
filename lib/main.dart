bb9e54f291ca214bd6e25954df77c78dc5911325
Mudaser <mudaserrahman7@gmail.com>
2026-07-31 15:28:19 +0500

    now comeback after long time

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'core/config/app_routes.dart';
+import 'features/auth/presentation/auth_bloc.dart';
+import 'features/auth/presentation/auth_event.dart';
+import 'features/auth/presentation/auth_pages/home.dart';
+import 'features/auth/presentation/auth_pages/onboarding_page.dart';
+import 'features/auth/presentation/auth_pages/signIn.dart';
+import 'features/auth/presentation/auth_pages/signUp.dart';
+import 'features/auth/presentation/auth_pages/splash_screen.dart';
+
 import 'injection_container.dart' as di;
 
 Future<void> main() async {
-  // 1. Initialize Flutter bindings (Required for async work before runApp)
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 2. Load the .env file
+  // Load .env
   await dotenv.load(fileName: ".env");
 
-  // 3. Initialize Supabase using the keys from .env
+  // Initialize Supabase
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL'] ?? '',
     anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
   );
 
-  // 4. Initialize Dependency Injection
+  // Initialize GetIt
   await di.init();
 
-  // 5. Run the app
   runApp(const MyApp());
 }
 
@@ -28,16 +37,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Coffee App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.brown,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Coffee App is running! '),
+    return BlocProvider(
+      create: (_) => di.sl<AuthBloc>()..add(GetCurrentUserEvent()),
+      child: MaterialApp(
+        title: 'Coffee App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.brown,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown),
+          useMaterial3: true,
         ),
+        initialRoute: AppRoutes.splash,
+        routes: {
+          AppRoutes.splash: (context) => const SplashScreeen(),
+          AppRoutes.onboarding: (context) => const OnboardingPage(),
+          // AppRoutes.signIn: (context) => const SignInPage(),
+          // AppRoutes.signUp: (context) => const SignUpPage(),
+          // AppRoutes.home: (context) => const HomePage(),
+        },
       ),
     );
   }
