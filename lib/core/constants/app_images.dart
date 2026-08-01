7a78da0fdc7e52ab25797df6089c8c016afbd3a3
Mudaser <mudaserrahman7@gmail.com>
2026-08-01 15:09:18 +0500

    now set the dashboard well

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,7 +1,7 @@
 class AppImages {
-  static const onboarding1 = 'assets/onboarding/onb1.PNG';
-  static const onboarding2 = 'assets/onboarding/onb2.PNG';
-  static const onboarding3 = 'assets/onboarding/onb3.PNG';
+  static const onboarding1 = 'assets/onboarding/ona1.PNG';
+  static const onboarding2 = 'assets/onboarding/ona2.PNG';
+  static const onboarding3 = 'assets/onboarding/ona3.PNG';
 
   static const splashLogo = 'assets/splash/splash_logo.png';
 }
