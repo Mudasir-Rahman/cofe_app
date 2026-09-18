36e14818c44d2d1fedbcf5f1407a5e85748bea0a
Mudaser <mudaserrahman7@gmail.com>
2026-09-18 10:43:51 +0500

    now work on event

--- lib/features/profile/presentation/bloc/profile_event.dart
+++ lib/features/profile/presentation/bloc/profile_event.dart
@@ -19,3 +19,25 @@ String? avatarUrl;
 @override
 List<Object?> get props => [fullName,phoneNumber,avatarUrl];
 }
+class UpdateProfileEvent extends ProfileEvent{
+  String? fullName ;
+  String? phoneNumber;
+  String? avatarUrl;
+  UpdateProfileEvent({
+    this.fullName,
+    this.phoneNumber,
+    this.avatarUrl,
+  });
+@override
+  List<Object?> get props => [fullName,phoneNumber,avatarUrl];
+}
+class DeleteProfileEvent extends ProfileEvent{
+  DeleteProfileEvent();
+  @override
+  List<Object?> get props => [];
+}
+class GetProfileEvent extends ProfileEvent{
+  GetProfileEvent();
+  @override
+  List<Object?> get props => [];
+}
